936ff37e36870919378a5a994781af04e86fce2f
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-10-01 00:48:07 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 #display_rainbow(Array)
-
+color_letter = ["R:","O","Y","G","B","I","V"]
 def display_rainbow(colors)
   colors.each do |color|
     puts "#{color}"
