ec4f67e5d01533d3ec0b8d5e5fe1fbe98760f181
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-10-01 02:22:45 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,8 @@
 #display_rainbow(Array)
 def display_rainbow(colors)
   
-  puts "R: #{colors[0]}","R: #{colors[0]}","R: #{colors[0]}",
+  puts "R: #{colors[0]}","R: #{colors[0]}","R: #{colors[0]}","R: #{colors[0]}",
+              
+  
     
 end 
