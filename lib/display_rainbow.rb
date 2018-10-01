522616f7f09ea2521b036c51249594697f96b656
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-10-01 02:19:32 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-#display_rainbow(Array)
+#display_rainbow(Array1, Array2)
 color_letter = ["R:","O:","Y:","G:","B:","I:","V:"]
 def display_rainbow(colors, color_letter)
   colors.each do |color|
